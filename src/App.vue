50088c2c667c44e3663a235244b72ca768b66d66
lihaojie <lihaojie@keytop.com.cn>
2024-11-25 15:47:23 +0800

    feat: commit2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-
+console.log("我准备撤回的commit2");
 </script>
 
 <template>
-    <router-view />
+  <router-view />
 </template>
 <style>
-body,html {
+body,
+html {
   margin: 0;
   padding: 0;
 }
